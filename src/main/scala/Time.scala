b87dc31f3bea60b4b747e02b3083c537bbf9d142
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-12 14:16:09 +0200

    OO basics – operators

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -2,6 +2,8 @@ class Time(val hours: Int, val minutes: Int) {
   // TODO Check preconditions: hours must be within [0, 24)!
   // TODO Check preconditions: minutes must be within [0, 60)!
 
+  def -(that: Time): Int = minus(that)
+
   def minus(that: Time): Int = {
     def asMinutes(time: Time) = time.hours * 60 + time.minutes
     asMinutes(this) - asMinutes(that)
