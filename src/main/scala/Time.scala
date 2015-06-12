8d2fd492db30dc200e6c177467a162b24d20777f
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-12 14:17:36 +0200

    OO basics – named and default arguments

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-final class Time(val hours: Int, val minutes: Int) {
+final class Time(val hours: Int = 0, val minutes: Int = 0) {
   // TODO Check preconditions: hours must be within [0, 24)!
   // TODO Check preconditions: minutes must be within [0, 60)!
 
