9c5123075f69d4b6df3dfcfb8ccacbb0ebb8c33a
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-12 14:27:46 +0200

    OO basics – case classes

--- src/main/scala/de/heikoseeberger/scalatrain/Time.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/Time.scala
@@ -4,7 +4,7 @@ object Time {
   def fromMinutes(minutes: Int): Time = new Time(minutes / 60, minutes % 60)
 }
 
-class Time(val hours: Int = 0, val minutes: Int = 0) {
+case class Time(hours: Int = 0, minutes: Int = 0) {
   // TODO Check preconditions: hours must be within [0, 24)!
   // TODO Check preconditions: minutes must be within [0, 60)!
 
