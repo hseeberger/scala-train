28aa3961cbea6f7a477fbc786634f7611747f255
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-12 14:25:13 +0200

    OO basics – singleton objects

--- src/main/scala/de/heikoseeberger/scalatrain/Time.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/Time.scala
@@ -16,6 +16,11 @@
 
 package de.heikoseeberger.scalatrain
 
+object Time {
+  def fromMinutes(minutes: Int): Time =
+    new Time(minutes / 60, minutes % 60)
+}
+
 final class Time(val hours: Int = 0, val minutes: Int = 0) {
   // TODO Check preconditions: hours must be within [0, 24)!
   // TODO Check preconditions: minutes must be within [0, 60)!
