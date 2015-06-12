98ab58a15e2cf663ab54ad4c13792c04c7835b05
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-12 14:13:29 +0200

    OO basics – methods

---
+++ src/main/scala/Time.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+final class Time(val hours: Int, val minutes: Int) {
+  // TODO Check preconditions: hours must be within [0, 24)!
+  // TODO Check preconditions: minutes must be within [0, 60)!
+
+  def minus(that: Time): Int = {
+    val thisAsMinutes = hours * 60 + minutes
+    val thatAsMinutes = that.hours * 60 + that.minutes
+    thisAsMinutes - thatAsMinutes
+  }
+}
