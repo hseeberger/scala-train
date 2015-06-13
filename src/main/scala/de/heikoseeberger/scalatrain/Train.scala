5bde987892527de1860a33f3ba8690ed157ba590
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-13 13:40:59 +0200

    Collections

--- src/main/scala/de/heikoseeberger/scalatrain/Train.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/Train.scala
@@ -1,5 +1,13 @@
 package de.heikoseeberger.scalatrain
 
-case class Train(kind: String, number: Int) {
+import scala.collection.immutable.Seq
+
+case class Train(kind: String, number: Int, schedule: Seq[Station]) {
   require(kind.nonEmpty, "kind must not be empty!")
+  require(schedule.size >= 2, "schedule must have at least two stops!")
+  require(schedule.distinct == schedule, "schedule must not contain duplicate stations!")
+}
+
+case class Station(name: String) {
+  require(name.nonEmpty, "name must not be empty!")
 }
