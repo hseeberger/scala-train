716845948c57bda93bb9484b9ca0f01b4236f9ad
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-13 13:52:33 +0200

    FP basics – map

--- src/main/scala/de/heikoseeberger/scalatrain/Train.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/Train.scala
@@ -2,10 +2,17 @@ package de.heikoseeberger.scalatrain
 
 import scala.collection.immutable.Seq
 
-case class Train(kind: String, number: Int, schedule: Seq[Station]) {
+case class Train(kind: String, number: Int, schedule: Seq[Stop]) {
   require(kind.nonEmpty, "kind must not be empty!")
   require(schedule.size >= 2, "schedule must have at least two stops!")
-  require(schedule.distinct == schedule, "schedule must not contain duplicate stations!")
+  require(stations.distinct == stations, "schedule must not contain duplicate stations!")
+  // TODO Check precondition: schedule must be increasing in time!
+
+  def stations: Seq[Station] = schedule.map(_.station)
+}
+
+case class Stop(station: Station, arrivalTime: Time, departureTime: Time) {
+  // TODO Check precondition: arrivalTime must be before departureTime!
 }
 
 case class Station(name: String) {
