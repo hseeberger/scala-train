dc8c8dfb086af84b6e5ded6783d9dca07c6eb501
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-13 14:15:13 +0200

    FP basics – for expressions

--- src/main/scala/de/heikoseeberger/scalatrain/JourneyPlanner.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/JourneyPlanner.scala
@@ -5,4 +5,9 @@ class JourneyPlanner(trains: Set[Train]) {
   def stations: Set[Station] = trains.flatMap(_.stations)
 
   def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
+
+  def departuresAt(station: Station): Set[(Time, Train)] = for {
+    train <- trains
+    stop <- train.schedule if stop.station == station
+  } yield (stop.departureTime, train)
 }
