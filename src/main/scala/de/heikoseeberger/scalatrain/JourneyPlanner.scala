8b94e9fac0c64353a0969d251f94cca7e7745b70
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-13 14:12:50 +0200

    FP basics – filter

--- src/main/scala/de/heikoseeberger/scalatrain/JourneyPlanner.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/JourneyPlanner.scala
@@ -1,5 +1,8 @@
 package de.heikoseeberger.scalatrain
 
 class JourneyPlanner(trains: Set[Train]) {
+
   def stations: Set[Station] = trains.flatMap(_.stations)
+
+  def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
 }
