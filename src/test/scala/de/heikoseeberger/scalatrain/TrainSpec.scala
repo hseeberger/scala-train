7fb78205b1ab3de51b2ea8f1c03ace9eddd6eba5
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-15 09:24:47 +0200

    Inheritance and mix-ins – ADTs

--- src/test/scala/de/heikoseeberger/scalatrain/TrainSpec.scala
+++ src/test/scala/de/heikoseeberger/scalatrain/TrainSpec.scala
@@ -6,33 +6,20 @@ class TrainSpec extends WordSpec with Matchers {
   import TestData._
 
   "Creating a Train" should {
-    "throw an IllegalArgumentException for an empty kind" in {
-      an[IllegalArgumentException] should be thrownBy Train(
-        "",
-        666,
-        List(
-          Stop(scalaCity, re666ScalaCity._1, re666ScalaCity._2),
-          Stop(slickMountain, re666SlickMountain._1, re666SlickMountain._2)
-        )
-      )
-    }
-
     "throw an IllegalArgumentException for an empty schedule" in {
-      an[IllegalArgumentException] should be thrownBy Train("RegionalExpress", 666, Nil)
+      an[IllegalArgumentException] should be thrownBy Train(TrainInfo.RegionalExpress(666), Nil)
     }
 
     "throw an IllegalArgumentException for a schedule with one stop" in {
       an[IllegalArgumentException] should be thrownBy Train(
-        "RegionalExpress",
-        666,
+        TrainInfo.RegionalExpress(666),
         List(Stop(scalaCity, re666ScalaCity._1, re666ScalaCity._2))
       )
     }
 
     "throw an IllegalArgumentException for a schedule with duplicate Stations" in {
       an[IllegalArgumentException] should be thrownBy Train(
-        "RegionalExpress",
-        666,
+        TrainInfo.RegionalExpress(666),
         List(
           Stop(scalaCity, re666ScalaCity._1, re666ScalaCity._2),
           Stop(scalaCity, re666SlickMountain._1, re666SlickMountain._2)
