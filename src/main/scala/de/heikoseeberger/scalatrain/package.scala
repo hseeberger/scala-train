4c333deea98c39f42186757e95685f19926bf0b8
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-15 22:54:19 +0200

    Advanced concepts – context bounds

--- src/main/scala/de/heikoseeberger/scalatrain/package.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/package.scala
@@ -5,16 +5,16 @@ import scala.collection.immutable.Seq
 
 package object scalatrain {
 
-  def isIncreasing[A <: Ordered[A]](as: Seq[A]): Boolean = as
+  def isIncreasing[A: Ordering](as: Seq[A]): Boolean = as
     .sliding(2) // scalacheck:off
     .forall {
-      case Seq(a1, a2) => a1 < a2
+      case Seq(a1, a2) => implicitly[Ordering[A]].lt(a1, a2)
       case _           => true
     }
 
   @tailrec
-  def isIncreasingTailrec[A <: Ordered[A]](as: Seq[A]): Boolean = as match {
-    case Seq(a1, a2, _*) => a1 < a2 && isIncreasingTailrec(as.tail)
+  def isIncreasingTailrec[A: Ordering](as: Seq[A]): Boolean = as match {
+    case Seq(a1, a2, _*) => implicitly[Ordering[A]].lt(a1, a2) && isIncreasingTailrec(as.tail)
     case _               => true
   }
 }
