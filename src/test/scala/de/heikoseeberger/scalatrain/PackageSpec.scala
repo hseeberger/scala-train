48c5886921abecca0fa96f05f26745c6144d1805
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-15 22:17:14 +0200

    Advanced concepts – upper bounds

---
+++ src/test/scala/de/heikoseeberger/scalatrain/PackageSpec.scala
@@ -0,0 +1,28 @@
+package de.heikoseeberger.scalatrain
+
+import org.scalatest.{ Matchers, WordSpec }
+
+class PackageSpec extends WordSpec with Matchers {
+
+  "Calling isIncreasing(Tailrec)" should {
+    "return true for an empty sequence" in {
+      isIncreasing(Nil) shouldBe true
+      isIncreasingTailrec(Nil) shouldBe true
+    }
+
+    "return true for a sequence with one element" in {
+      isIncreasing(List(Time())) shouldBe true
+      isIncreasingTailrec(List(Time())) shouldBe true
+    }
+
+    "return true for an increasing sequence" in {
+      isIncreasing(List(Time(1), Time(2), Time(3))) shouldBe true
+      isIncreasingTailrec(List(Time(1), Time(2), Time(3))) shouldBe true
+    }
+
+    "return false for an non-increasing sequence" in {
+      isIncreasing(List(Time(1), Time(2), Time(2), Time(3))) shouldBe false
+      isIncreasingTailrec(List(Time(1), Time(2), Time(2), Time(3))) shouldBe false
+    }
+  }
+}
