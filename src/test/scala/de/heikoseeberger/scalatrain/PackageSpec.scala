e3e3acbcef89a502846ca47a4a01621de2f9cfcf
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-15 22:54:19 +0200

    Advanced concepts – context bounds

--- src/test/scala/de/heikoseeberger/scalatrain/PackageSpec.scala
+++ src/test/scala/de/heikoseeberger/scalatrain/PackageSpec.scala
@@ -6,23 +6,27 @@ class PackageSpec extends WordSpec with Matchers {
 
   "Calling isIncreasing(Tailrec)" should {
     "return true for an empty sequence" in {
-      isIncreasing(Nil) shouldBe true
-      isIncreasingTailrec(Nil) shouldBe true
+      isIncreasing(List.empty[Time]) shouldBe true
+      isIncreasingTailrec(List.empty[Time]) shouldBe true
+      isIncreasingTailrec(List.empty[Int]) shouldBe true
     }
 
     "return true for a sequence with one element" in {
       isIncreasing(List(Time())) shouldBe true
       isIncreasingTailrec(List(Time())) shouldBe true
+      isIncreasingTailrec(List(1)) shouldBe true
     }
 
     "return true for an increasing sequence" in {
       isIncreasing(List(Time(1), Time(2), Time(3))) shouldBe true
       isIncreasingTailrec(List(Time(1), Time(2), Time(3))) shouldBe true
+      isIncreasingTailrec(List(1, 2, 3)) shouldBe true
     }
 
     "return false for an non-increasing sequence" in {
       isIncreasing(List(Time(1), Time(2), Time(2), Time(3))) shouldBe false
       isIncreasingTailrec(List(Time(1), Time(2), Time(2), Time(3))) shouldBe false
+      isIncreasingTailrec(List(1, 2, 2, 3)) shouldBe false
     }
   }
 }
