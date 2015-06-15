8cbc251cef442bdf4eadd09d696240dccc8282a8
Heiko Seeberger <mail@heikoseeberger.de>
2015-06-15 09:24:47 +0200

    Inheritance and mix-ins – ADTs

--- src/main/scala/de/heikoseeberger/scalatrain/Train.scala
+++ src/main/scala/de/heikoseeberger/scalatrain/Train.scala
@@ -2,13 +2,22 @@ package de.heikoseeberger.scalatrain
 
 import scala.collection.immutable.Seq
 
-case class Train(kind: String, number: Int, schedule: Seq[Stop]) {
-  require(kind.nonEmpty, "kind must not be empty!")
+case class Train(info: TrainInfo, schedule: Seq[Stop]) {
   require(schedule.size >= 2, "schedule must have at least two stops!")
   require(stations.distinct == stations, "schedule must not contain duplicate stations!")
   // TODO Check precondition: schedule must be increasing in time!
 
   def stations: Seq[Station] = schedule.map(_.station)
+}
+
+sealed abstract class TrainInfo {
+  def number: Int
+}
+
+object TrainInfo {
+  case class InterCityExpress(number: Int, hasWifi: Boolean = false) extends TrainInfo
+  case class InterCity(number: Int) extends TrainInfo
+  case class RegionalExpress(number: Int) extends TrainInfo
 }
 
 case class Stop(station: Station, arrivalTime: Time, departureTime: Time) {
