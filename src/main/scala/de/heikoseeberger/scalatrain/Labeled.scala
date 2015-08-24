4e54f06dd6dde887ef0280dc650b9b99db8181c0
Heiko Seeberger <mail@heikoseeberger.de>
2015-08-24 12:07:29 +0200

    Implicits – implicit parameters

---
+++ src/main/scala/de/heikoseeberger/scalatrain/Labeled.scala
@@ -0,0 +1,23 @@
+package de.heikoseeberger.scalatrain
+
+import java.util.{ Locale, MissingResourceException, ResourceBundle }
+
+object Labeled {
+
+  implicit object StationLabeled extends Labeled[Station] {
+    override def label(station: Station)(implicit locale: Locale) = lookupLabel(s"station.${station.name}")
+  }
+
+  def label[A](a: A)(implicit labeled: Labeled[A], locale: Locale): String = labeled.label(a)
+
+  private def lookupLabel(key: String)(implicit locale: Locale) =
+    try
+      ResourceBundle.getBundle("labels", locale).getString(key)
+    catch {
+      case _: MissingResourceException => key
+    }
+}
+
+trait Labeled[A] {
+  def label(a: A)(implicit locale: Locale): String
+}
